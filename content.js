795b33aa556faeac0a49b177fd7ed46ddf914079
Pablo Santibañez Jara <kaishos@gmail.com>
2025-07-23 15:23:42 +0200

    forgot this

--- content.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('keydown', (e) => {
-    if (e.ctrlKey && e.code === 'Space') {
-        localStorage.setItem('tabFreezerOverride', 'true');
-        setTimeout(() => localStorage.removeItem('tabFreezerOverride'), 3000);
-    }
-});
